8f632a8864e8c0e4f89c146dff8ccaa73baf3a70
Lincan Li <llcan1120@gmail.com>
2014-11-01 01:38:19 -0400

    merge stripeFunction

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,6 @@
 require("cloud/app.js");
+require("cloud/stripeFunction.js");
+
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
