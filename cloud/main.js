9abe192d67a57003005ccfc6ef3a600c6589687d
Lincan Li <llcan1120@gmail.com>
2014-11-01 01:39:00 -0400

    merge stripeFunction fix error

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,27 @@
 require("cloud/app.js");
-require("cloud/stripeFunction.js");
 
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
-AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+var stripe = require("stripe")( "sk_test_sr52IMYk7RCWAEIhZhmQh21L" );
+
+// (Assuming you're using express - expressjs.com)
+// Get the credit card details submitted by the form
+
+AV.Cloud.define("stripeCharge", function(request, response) {
+
+  	var stripeToken = request.object.get("stripeToken");
+
+	var charge = stripe.charges.create({
+ 		amount: 1000, // amount in cents, again
+  		currency: "usd",
+  		card: stripeToken,
+  		description: "payinguser@example.com"
+	}, function(err, charge) {
+  		if (err && err.type === 'StripeCardError') {
+    		response.error("declined");
+  		}
+  		response.success(charge);
+
+
+	});
+
+
 });
--- cloud/views/stripeFunction.js
+++ cloud/views/stripeFunction.js
@@ -1,29 +1,2 @@
 require("cloud/app.js");
 
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here https://dashboard.stripe.com/account
-var stripe = require("stripe")( "sk_test_sr52IMYk7RCWAEIhZhmQh21L" );
-
-// (Assuming you're using express - expressjs.com)
-// Get the credit card details submitted by the form
-
-AV.Cloud.define("stripeCharge", function(request, response) {
-
-  	var stripeToken = request.object.get("stripeToken");
-
-	var charge = stripe.charges.create({
- 		amount: 1000, // amount in cents, again
-  		currency: "usd",
-  		card: stripeToken,
-  		description: "payinguser@example.com"
-	}, function(err, charge) {
-  		if (err && err.type === 'StripeCardError') {
-    		response.error("declined");
-  		}
-  		response.success(charge);
-
-
-	});
-
-
-});
