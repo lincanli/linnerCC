6a02716c870760b7c27931a5766008a4a247ba14
Lincan Li <llcan1120@gmail.com>
2014-11-01 01:22:43 -0400

    sepearte stripe file

---
+++ cloud/views/stripeFunction.js
@@ -0,0 +1,20 @@
+require("cloud/app.js");
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here https://dashboard.stripe.com/account
+var stripe = require("stripe")( "sk_test_sr52IMYk7RCWAEIhZhmQh21L" );
+
+// (Assuming you're using express - expressjs.com)
+// Get the credit card details submitted by the form
+var stripeToken = request.body.stripeToken;
+
+var charge = stripe.charges.create({
+  amount: 1000, // amount in cents, again
+  currency: "usd",
+  card: stripeToken,
+  description: "payinguser@example.com"
+}, function(err, charge) {
+  if (err && err.type === 'StripeCardError') {
+    // The card has been declined
+  }
+});
