1627e586166f4622b90c63a10be16dabd15e6a0f
Lincan Li <llcan1120@gmail.com>
2014-11-01 01:27:04 -0400

    add stripe function

--- cloud/views/stripeFunction.js
+++ cloud/views/stripeFunction.js
@@ -6,15 +6,22 @@ var stripe = require("stripe")( "sk_test_sr52IMYk7RCWAEIhZhmQh21L" );
 
 // (Assuming you're using express - expressjs.com)
 // Get the credit card details submitted by the form
-var stripeToken = request.body.stripeToken;
 
-var charge = stripe.charges.create({
-  amount: 1000, // amount in cents, again
-  currency: "usd",
-  card: stripeToken,
-  description: "payinguser@example.com"
-}, function(err, charge) {
-  if (err && err.type === 'StripeCardError') {
-    // The card has been declined
-  }
+AV.Cloud.define("stripeCharge", function(request, response) {
+
+  	var stripeToken = request.object.get("stripeToken");
+
+	var charge = stripe.charges.create({
+ 		amount: 1000, // amount in cents, again
+  		currency: "usd",
+  		card: stripeToken,
+  		description: "payinguser@example.com"
+	}, function(err, charge) {
+  		if (err && err.type === 'StripeCardError') {
+    		response.error("declined");
+  		}
+
+	});
+
+
 });
